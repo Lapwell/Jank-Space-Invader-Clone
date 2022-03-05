42b15d79e407d2f94a4629404e89df3d76fbb114
Lapwell <76802132+Lapwell@users.noreply.github.com>
2022-03-04 23:24:29 -0800

    Completed:     -Set up window.     -FPS counter.
    To Do:
        -Create player objects.
        -Player projectiles.
        -Create various aliens.
        -Alien projectiles.
        -Implement hit reg.
        -Lives.
        -Player movement.
        -Alien movement.
        -Shields.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randrange
 import sys
 
 pygame.init()
@@ -13,18 +14,28 @@ WIDTH, HEIGHT = 800, 800
 FPS = 60
 VEl = 24
 
+# Pygame variables
 pygame.display.set_caption('Jank Space Invader Clone')
 ROOT = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 FONT = pygame.font.Font(None, 24)
 
 # Non-constant values
-
+enemy_list = []
+enemy_projectiles = []
+player_projectiles = []
 
 # Custom events
 
 
+# This function is for the chance of an enemy to shoot
+def enemy_shoot(chance):
+    return randrange(0, chance)
+
+
+# Here we check the various events we are looking out for in the program. Game events (death, level clears, mplayer input, etc).
 def check_events():
+    # This assigns a list of currents pressed keys to a "keys" variable, we then use this to check for player inputs and execute the relevant code.
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         pass
@@ -38,12 +49,15 @@ def check_events():
             sys.exit()
 
 
+# This function uses the assigned function on line 18 (clock = pygame.time.Clock()) to get the current frame.
+# Once we get the framce "count" we then draw the number on the top left of the window.
 def fps_counter():
     count = str(int(clock.get_fps()))
     fps_txt = FONT.render(count, True, WHITE)
     ROOT.blit(fps_txt, (fps_txt.get_width() - fps_txt.get_width()//2, fps_txt.get_height() - fps_txt.get_height()//2))
 
 
+# This function is used to update the window every frame.
 def draw_root():
     ROOT.fill(BLACK)
     fps_counter()
