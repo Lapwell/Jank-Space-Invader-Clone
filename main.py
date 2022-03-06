f7680963598d431bfbc115da3270dcb551aa70fb
Lapwell <76802132+Lapwell@users.noreply.github.com>
2022-03-06 00:13:49 -0800

    Completed:     -Set up window.     -FPS counter.
    To Do:
        -Create player objects.
        -Player projectiles.
        -Create various aliens.
        -Alien projectiles.
        -Implement hit reg.
        -Lives.
        -Player movement.
        -Alien movement.
    
    Optional:
        -Shields.

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import sys
 
 pygame.init()
 
-# Constant values
+# Constant variables
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 255, 0, 0
 GREEN = 0, 255, 0
 BLUE = 0, 0, 255
 WIDTH, HEIGHT = 800, 800
-FPS = 60
-VEl = 24
+FPS = 30
+SIZE = 32
 
 # Pygame variables
 pygame.display.set_caption('Jank Space Invader Clone')
@@ -20,27 +20,41 @@ ROOT = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 FONT = pygame.font.Font(None, 24)
 
-# Non-constant values
+# Non-constant variables
 enemy_list = []
 enemy_projectiles = []
 player_projectiles = []
 
-# Custom events
+
+# This class stores all player data, creates the game object for the player and generally just deals with the player.
+class PlayerClass:
+    def __init__(self, posx, size, lives):
+        self.posx = posx
+        self.size = size
+        self. lives = lives
+
+    def load_player(self):
+        return pygame.Rect(self.posx, (HEIGHT - SIZE * 2), self.size, self.size)
+
+    def update_pos(self, new_pos):
+        self.posx = self.posx - new_pos
 
 
-# This function is for the chance of an enemy to shoot
+# This function is for the chance of an enemy to shoot. Chance should be a float between 1 and 0.1
 def enemy_shoot(chance):
-    return randrange(0, chance)
+    num = randrange(0, 100)
+    return num * chance
 
 
-# Here we check the various events we are looking out for in the program. Game events (death, level clears, mplayer input, etc).
-def check_events():
+# Here we check the various events we are looking out for in the program.
+def check_events(player_obj):
     # This assigns a list of currents pressed keys to a "keys" variable, we then use this to check for player inputs and execute the relevant code.
+    VEL = 10
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        pass
+        player_obj.update_pos(VEL)
     if keys[pygame.K_RIGHT]:
-        pass
+        player_obj.update_pos(-VEL)
     if keys[pygame.K_SPACE]:
         pass
     for event in pygame.event.get():
@@ -58,16 +72,19 @@ def fps_counter():
 
 
 # This function is used to update the window every frame.
-def draw_root():
+def draw_root(player_obj):
     ROOT.fill(BLACK)
+    pygame.draw.rect(ROOT, GREEN, player_obj.load_player())
     fps_counter()
     pygame.display.update()
 
 
 def main():
+    screen_center = WIDTH - WIDTH//2 - SIZE//2
+    player_object = PlayerClass(screen_center, 20, 3)
     while True:
-        check_events()
-        draw_root()
+        check_events(player_object)
+        draw_root(player_object)
         clock.tick(FPS)
 
 
