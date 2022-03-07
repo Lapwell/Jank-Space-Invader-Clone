a4e972327e6d9fdd5f1550b4b019a900232f350e
Lapwell <76802132+Lapwell@users.noreply.github.com>
2022-03-06 23:43:16 -0800

    Completed:     -Set up window.     -FPS counter.     -Create player object.     -Player movement.     -Player projectiles.
    To Do:
       *-Aliens.
        -Alien projectiles.
        -Implement hit reg.
        -Lives.
        -Alien movement.
    
    Optional:
        -Shields.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ GREEN = 0, 255, 0
 BLUE = 0, 0, 255
 WIDTH, HEIGHT = 800, 800
 FPS = 30
-SIZE = 32
+SIZE = 20
 
 # Pygame variables
 pygame.display.set_caption('Jank Space Invader Clone')
@@ -22,37 +22,52 @@ FONT = pygame.font.Font(None, 24)
 
 # Non-constant variables
 enemy_list = []
-enemy_projectiles = []
-player_projectiles = []
-
-# Events
-
+projectiles = []
 
 
 # This class stores all player data, creates the game object for the player and generally just deals with the player.
 class PlayerClass:
-    def __init__(self, posx, size, lives):
+    def __init__(self, posx, posy, size, lives):
         self.posx = posx
+        self.posy = posy
         self.size = size
         self. lives = lives
 
     def load_player(self):
-        return pygame.Rect(self.posx, (HEIGHT - SIZE * 2), self.size, self.size)
+        return pygame.Rect(self.posx, self.posy, self.size, self.size)
 
     def update_pos(self, new_pos):
         self.posx = self.posx - new_pos
+
+
+# This class handles all the projectiles, both player and enemy
+class Projectiles:
+    def __init__(self, posx, posy, vel):
+        self.posx = posx
+        self.posy = posy
+        self.vel = vel
+
+    def draw_projectile(self):
+        return pygame.Rect(self.posx + SIZE//2.5, self.posy, 4, 8)
+
+    def move_projectile(self):
+        self.posy -= self.vel
+
+    def check_hit(self):
+        reference_list = list(projectiles)
+        index = reference_list.index(self)
+        reference_list.pop(index)
+        if self.posy < 0:
+            return True
+        for item in reference_list:
+            if self.posy == item.posy:
+                return True
 
 
 # This function is for the chance of an enemy to shoot. Chance should be a float between 1 and 0.1
 def enemy_shoot(chance):
     num = randrange(0, 100)
     return num * chance
-
-
-def spawn_projectile(pos_tuple, player):
-    if player:
-        player_projectiles.append(pos_tuple)
-        print(player_projectiles)
 
 
 # Here we check the various events we are looking out for in the program.
@@ -64,17 +79,17 @@ def check_events(player_obj):
         player_obj.update_pos(VEL)
     if keys[pygame.K_RIGHT]:
         player_obj.update_pos(-VEL)
-    if keys[pygame.KEYDOWN]:
-        if len(player_projectiles) < 10:
-            spawn_projectile((player_obj.posx, 2067), True)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and len(projectiles) < 10:
+                projectiles.append(Projectiles(player_obj.posx, player_obj.posy - 4, 8))
 
 
-# This function uses the assigned function on line 18 (clock = pygame.time.Clock()) to get the current frame.
-# Once we get the framce "count" we then draw the number on the top left of the window.
+# This creates the FPS counter at the top left
 def fps_counter():
     count = str(int(clock.get_fps()))
     fps_txt = FONT.render(count, True, WHITE)
@@ -84,16 +99,19 @@ def fps_counter():
 # This function is used to update the window every frame.
 def draw_root(player_obj):
     ROOT.fill(BLACK)
+    for item in projectiles:
+        if item.check_hit():
+            projectiles.pop(projectiles.index(item))
+        pygame.draw.rect(ROOT, WHITE, item.draw_projectile())
+        item.move_projectile()
     pygame.draw.rect(ROOT, GREEN, player_obj.load_player())
-    # for items in player_projectiles:
-    #     pygame.draw.rect(ROOT, WHITE, ())
     fps_counter()
     pygame.display.update()
 
 
 def main():
     screen_center = WIDTH - WIDTH//2 - SIZE//2
-    player_object = PlayerClass(screen_center, 20, 3)
+    player_object = PlayerClass(screen_center, HEIGHT - SIZE * 2, SIZE, 3)
     while True:
         check_events(player_object)
         draw_root(player_object)
