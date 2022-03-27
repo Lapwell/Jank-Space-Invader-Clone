8872f00a44558ba05ebbbd3172b636723079c830
Lapwell <76802132+Lapwell@users.noreply.github.com>
2022-03-27 06:33:42 -0700

    Completed:     -Set up window.     -FPS counter.     -Create player object.     -Player movement.     -Player projectiles.     -Implement hit reg.     -Aliens.     -Alien movement.     -Alien projectiles.     -Lives.
    To Do:
        -Add art work.
    
    Optional:
        -Shields.
        -Sound effects.
        -Music.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from random import randrange
+import random
 import sys
 
 pygame.init()
@@ -20,15 +20,31 @@ ROOT = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 FONT = pygame.font.Font(None, 24)  # The font for any text in-game
 screen_center = WIDTH - WIDTH // 2 - SIZE // 2
-ENEMY_MOVE_EVENT = 25  # This is an event to move the enemies
-EVENT_TIMER = 1000
-pygame.time.set_timer(ENEMY_MOVE_EVENT, EVENT_TIMER)
+
+# Other variables.
+game_over = False
+win = False
 direction = 1
 move_count = 0
+# Game over text
+game_over_text = FONT.render('GAME OVER', False, RED)
+game_over_screen = game_over_text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+# Win text
+win_text = FONT.render('YOU WIN', False, GREEN)
+win_screen = win_text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+
+# Custom pygame events.
+ENEMY_MOVE_EVENT = 25  # This is an event to move the enemies
+ENEMY_MOVE_TIMER = 1200
+pygame.time.set_timer(ENEMY_MOVE_EVENT, ENEMY_MOVE_TIMER)
+ENEMY_SHOOT_EVENT = 26
+ENEMY_SHOOT_TIMER = 1200
+pygame.time.set_timer(ENEMY_SHOOT_EVENT, ENEMY_SHOOT_TIMER)
 
 # These lists hold the references(?) for the class objects related to the list.
 enemy_list = []
 projectiles_list = []
+enemy_projectiles_list = []
 
 
 # This class stores all player data, creates the game object for the player and generally just deals with the player.
@@ -46,6 +62,12 @@ class PlayerClass:
     def update_pos(self, VEL):
         self.rect.x -= VEL
 
+    def check_health(self):
+        if self.lives <= 0:
+            global game_over
+            game_over = True
+            print('GAME OVER')
+
 
 class EnemyClass:
     def __init__(self, posx, posy):
@@ -59,18 +81,14 @@ class EnemyClass:
     def update_pos(self, VEL):
         self.rect.x -= VEL
 
-    def enemy_shoot(self, chance):
-        num = randrange(0, 100)
-        if num > chance:
-            projectiles_list.append(Projectiles(self.posx, self.posy, -8))
-
 
 # This class handles all the projectiles, both player and enemy
 class Projectiles:
-    def __init__(self, posx, posy, vel):
+    def __init__(self, posx, posy, vel, from_enemy):
         self.posx = posx
         self.posy = posy
         self.vel = vel
+        self.from_enemy = from_enemy
         self.rect = pygame.Rect(self.posx + SIZE//2.5, self.posy, 4, 8)
 
     # When called, this method spawns a projectile at a given coords (enemy or player position)
@@ -85,11 +103,15 @@ class Projectiles:
     def check_hit(self, player_obj):
         if self.rect.y < 0:
             projectiles_list.pop(projectiles_list.index(self))
+        if self.rect.y > HEIGHT:
+            enemy_projectiles_list.pop(enemy_projectiles_list.index(self))
         for item in enemy_list:
-            if item.rect.colliderect(self.rect):
+            if item.rect.colliderect(self.rect) and not self.from_enemy:
                 enemy_list.pop(enemy_list.index(item))
                 projectiles_list.pop(projectiles_list.index(self))
         if player_obj.rect.colliderect(self.rect):
+            print('Hit player')
+            enemy_projectiles_list.pop(enemy_projectiles_list.index(self))
             player_obj.lives -= 1
 
 
@@ -98,6 +120,7 @@ def check_events(player_obj):
     # This assigns a list of currents pressed keys to a "keys" variable, we then use this to check for player inputs and execute the relevant code.
     global direction
     global move_count
+    global game_over
     VEL = 4
     skip = False
     keys = pygame.key.get_pressed()
@@ -105,11 +128,17 @@ def check_events(player_obj):
         player_obj.update_pos(VEL)
     if keys[pygame.K_RIGHT] and player_obj.rect.x + SIZE < WIDTH:
         player_obj.update_pos(-VEL)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        # This event is to allow the enemies to shoot. Every time the event flag is raised, all enemies have a chance to shoot.
+        if event.type == ENEMY_SHOOT_EVENT:
+            for item in enemy_list:
+                chance = random.randrange(0, 101)
+                if chance > 96:
+                    enemy_projectiles_list.append(Projectiles(item.rect.x, item.rect.y, -4, True))
+        #  This event is about letting enemies move in steps, instead of a smooth slide across the screen.
         if event.type == ENEMY_MOVE_EVENT:
             if move_count >= 8:
                 direction = direction * -1
@@ -118,7 +147,7 @@ def check_events(player_obj):
                 for item in enemy_list:
                     item.rect.y += SIZE * 2
             for item in enemy_list:
-                if not skip:
+                if not skip:  # This skip is so that the enemies don't move down and right/left in one movement. The enmies move down then left/right in two movements.
                     skip = False
                     item.rect.x += VEL * 4 * direction
             move_count += 1
@@ -126,7 +155,7 @@ def check_events(player_obj):
         # This statement check if the spacebar is pressed and spawns on projectile for each press. Holding space doesn't spawn a ton of projectiles all at once.
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and len(projectiles_list) < 10:
-                projectiles_list.append(Projectiles(player_obj.rect.x, player_obj.rect.y - SIZE / 2, 4))
+                projectiles_list.append(Projectiles(player_obj.rect.x, player_obj.rect.y - SIZE / 2, 4, False))
 
 
 # This creates the FPS counter at the top left.
@@ -145,6 +174,13 @@ def draw_root(player_obj):
     for item in projectiles_list:
         item.check_hit(player_obj)
         pygame.draw.rect(ROOT, WHITE, item.draw_projectile())
+        item.move_projectile()
+    if len(enemy_list) == 0:
+        global win
+        win = True
+    for item in enemy_projectiles_list:
+        pygame.draw.rect(ROOT, WHITE, item.draw_projectile())
+        item.check_hit(player_obj)
         item.move_projectile()
     for item in enemy_list:
         pygame.draw.rect(ROOT, RED, item.get_enemy())
@@ -171,7 +207,28 @@ def main():
             z -= 1
 
     while True:
+        if game_over:
+            while True:
+                ROOT.fill(BLACK)
+                ROOT.blit(game_over_text, game_over_screen)
+                pygame.display.update()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                clock.tick(FPS)
+        if win:
+            while True:
+                ROOT.fill(BLACK)
+                ROOT.blit(win_text, win_screen)
+                pygame.display.update()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                clock.tick(FPS)
         check_events(player_object)
+        player_object.check_health()
         draw_root(player_object)
         clock.tick(FPS)
 
